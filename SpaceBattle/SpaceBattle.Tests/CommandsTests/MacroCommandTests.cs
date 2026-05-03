86902c5f5cb166d0e7c280b0aac8f3df8d8d761a
Chaizee <zuzindima26@gmail.com>
2026-05-03 13:37:35 +0600

    feat: add macrocommand realisation and tests

---
+++ SpaceBattle/SpaceBattle.Tests/CommandsTests/MacroCommandTests.cs
@@ -0,0 +1,41 @@
+using System;
+using SpaceBattle.Lib;
+using NSubstitute;
+
+namespace SpaceBattle.Lib.Tests.CommandsTests
+{
+    public class MacroCommandTests
+    {
+        [Fact]
+        public void Execute_AllCommandsExecuted()
+        {
+            var command1 = Substitute.For<ICommand>();
+            var command2 = Substitute.For<ICommand>();
+
+            var macro = new MacroCommand(new[] {command1, command2});
+        
+            macro.Execute();
+
+            command1.Received(1).Execute();
+            command2.Received(1).Execute();
+        }
+
+        [Fact]
+        public void Execute_WhenCommandThrowsException_DoesNotExecuteRemainingCommands()
+        {
+            var command1 = Substitute.For<ICommand>();
+            var command2 = Substitute.For<ICommand>();
+            var command3 = Substitute.For<ICommand>();
+
+            command2.When(x => x.Execute()).Do(x => {throw new Exception("Error"); });
+
+            var macro = new MacroCommand(new[] {command1, command2, command3});
+
+            Assert.Throws<Exception>(() => macro.Execute());
+
+            command1.Received(1).Execute();
+            command2.Received(1).Execute();
+            command3.DidNotReceive().Execute();
+        }
+    }
+}
